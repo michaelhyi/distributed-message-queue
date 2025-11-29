06e9e005f746bc1eb8c14ba75198b85a6d89ccac
michaelhyi <michaelyi2004@gmail.com>
2025-11-29 18:29:41 -0500

    updated docker img name

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t dmq .
-docker run -it -v "$(pwd)":/root -w /root dmq
+docker build -t distributed-message-queue .
+docker run -it -v "$(pwd)":/root -w /root distributed-message-queue
