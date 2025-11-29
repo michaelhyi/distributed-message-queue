7d6aabb2444c7be00f0c742719b759c2c892583a
michaelhyi <michaelyi2004@gmail.com>
2025-11-29 18:27:34 -0500

    created separate include dir for header files

---
+++ src/include/queue.h
@@ -0,0 +1,40 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+struct queue {
+    struct queue_node *head;
+    struct queue_node *tail;
+};
+
+struct queue_node {
+    char *data;
+    unsigned long long timestamp;
+
+    struct queue_node *next;
+};
+
+/**
+ * Initializes the queue.
+ * 
+ * @returns -1 if error
+ */
+int queue_init(struct queue *queue);
+
+/**
+ * Pushes data on the queue.
+ * 
+ * @param queue the queue to update
+ * @param data data to push on the queue
+ * @returns 0 if success, -1 if error
+ */
+int queue_push(struct queue *queue, char *data);
+
+/**
+ * Pops data off the queue.
+ * 
+ * @param queue the queue to update
+ * @returns the data popped from the queue. `NULL` if error or empty queue
+ */
+char *queue_pop(struct queue *queue);
+
+#endif
