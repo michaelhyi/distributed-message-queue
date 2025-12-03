4c1a77a71d6d7a6576c319c47d4128acbbcc5793
michaelhyi <michaelyi2004@gmail.com>
2025-12-03 00:06:47 -0500

    integrated getopt

--- src/main.c
+++ src/main.c
@@ -1,14 +1,23 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 
 #include "network.h"
 
 int main(int argc, char *argv[]) {
     unsigned int server_port = DEFAULT_SERVER_PORT;
 
-    // TODO: use getopts
-    if (argc >= 3 && strcmp(argv[1], "-p") == 0) {
-        server_port = atoi(argv[2]);
+    int opt;
+    while ((opt = getopt(argc, argv, "p:")) != -1) {
+        switch (opt) {
+            case 'p':
+                server_port = atoi(optarg);
+                break;
+            case '?':
+                return 1;
+            default:
+                break;
+        }
     }
 
     int res = server_init(server_port);
