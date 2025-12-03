10b7d9c17d8df30edd7044a07d4ff5ac503249c9
michaelhyi <michaelyi2004@gmail.com>
2025-12-02 20:28:51 -0500

    created simple TCP server

--- include/network.h
+++ include/network.h
@@ -1,21 +1,42 @@
 #ifndef NETWORK_H
 #define NETWORK_H
 
+/**
+ * TODO:
+ * 1. get small server working first
+ * 2. errno
+ * 3. clean up resources
+ * 4. get small client working first
+ * 5. unit testing?
+ * 6. timeouts
+ * 7. signal handling
+ * 8. heartbeats
+ */
+
 #define DEFAULT_SERVER_PORT 8080
 #define MAX_PAYLOAD_LENGTH 1 * MB
 #define MB 1 << 20
-#define NUM_REQUEST_HANDLER_THREADS 5
+#define ENCRYPTED_FLAG 0x1
 
 enum message_type {
     PUSH,
     POP,
+    PEEK,
     HEARTBEAT,
     HEARTBEAT_ACK
 };
 
+/**
+ * flags:
+ * ----------------------
+ * | 31 - 1 |     0     |
+ * ----------------------
+ * | unused | encrypted |
+ * ----------------------
+ */
 struct message_header {
     enum message_type type;
-    int encrypted;
+    int flags;
     unsigned int length;
 };
 
@@ -25,34 +46,38 @@ struct message {
 };
 
 /**
- * Initializes a TCP client connection to a server using sockets.
+ * Initializes a TCP client connection to a server.
  * 
+ * @param server_address the server address
+ * @param server_port the server port
  * @returns the socket file descriptor for the client connection, -1 if error
+ * with global `errno` set
  */
 int init_client(const char *server_address, unsigned int server_port);
 
 /**
- * Initializes a TCP server using sockets.
+ * Initializes a TCP server.
  * 
- * @param server_port the port to bind
- * @returns 0 on success, -1 on error
+ * @param server_port the port to bind the server to
+ * @returns 0 on success, -1 on error with global `errno` set
  */
 int init_server(unsigned int server_port);
 
 /**
  * Sends a message to a TCP connection.
  * 
- * @param client_socket TCP connection to send message to
+ * @param conn_socket TCP connection to send message to
  * @param message the message to send
- * @returns 0 if success, -1 if error
+ * @returns 0 if success, -1 if error with global `errno` set
  */
-int send_message(int client_socket, struct message message);
+int send_message(int conn_socket, struct message message);
 
 /**
  * Receives a message from a TCP connection.
  * 
  * @param conn_socket TCP connection to receive message from
- * @returns the message received at the server, `NULL` if error
+ * @returns the message received at the server, `NULL` if error with global
+ * `errno` set
  */
 struct message *receive_message(int conn_socket);
 
--- src/network.c
+++ src/network.c
@@ -1,79 +1,65 @@
 #include "network.h"
 
+#include <errno.h>
 #include <netinet/in.h>
 #include <pthread.h>
-#include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <unistd.h>
 
-static volatile sig_atomic_t stop = 0;
-static int server_socket = -1;
+/**
+ * Reads bytes into a buffer from a TCP stream.
+ * 
+ * @param fd file descriptor to read from
+ * @param buf buffer to write to
+ * @param length number of bytes to read
+ * @returns 0 if success, -1 if error with global `errno` set
+ */
+static int read_stream(int fd, void *buf, unsigned int length) {
+    unsigned int total = 0;
 
-static void handle_signal(int sig) {
-    (void) sig; // suppress unused param error
+    while (total < length) {
+        int n = read(fd, (char *) buf + total, length - total);
+        if (n <= 0) {
+            return -1;
+        }
 
-    stop = 1;
-
-    if (server_socket >= 0) {
-        close(server_socket);
-        server_socket = -1;
+        total += n;
     }
+
+    return 0;
 }
 
-static void init_signals() {
-    struct sigaction sa;
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = handle_signal;
-    sigfillset(&sa.sa_mask);
-    sa.sa_flags = 0;
+/**
+ * Listens on a TCP connection.
+ * 
+ * @param arg pointer to connection socket. must be freed once locally copied
+ */
+static void *connection_handler(void *arg) {
+    int conn_socket = *(int *) arg;
+    free(arg);
 
-    sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGTERM, &sa, NULL);
-}
-
-static void *handle_requests(void *arg) {
-    (void) arg; // suppress unused param error
-
-    while (!stop) {
-        struct sockaddr_in client_address;
-        socklen_t address_len = sizeof(client_address);
-
-        int conn_socket = accept(server_socket, (struct sockaddr *) &client_address, &address_len);
-        if (conn_socket < 0) {
-            continue;
-        }
-
-        struct timeval tv;
-        tv.tv_sec = 30;
-        tv.tv_usec = 0;
-        int res = setsockopt(conn_socket, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-        if (res < 0) {
-            close(conn_socket);
-            continue;
-        }
-
+    while (1) {
         struct message *message = receive_message(conn_socket);
         if (message == NULL) {
-            close(conn_socket);
             continue;
         }
 
-        // TODO: persist conn --> heartbeats?
         // TODO: do something with message
+        printf("received message: %s\n", (const char *) message->payload);
+
         free(message->payload);
         free(message);
     }
 
+    // TODO: close conn_socket
     return NULL;
 }
 
 int init_server(unsigned int server_port) {
-    init_signals();
-
-    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket < 0) {
         return -1;
     }
@@ -97,30 +83,27 @@ int init_server(unsigned int server_port) {
         return -1;
     }
 
-    pthread_t threads[NUM_REQUEST_HANDLER_THREADS];
-    for (int i = 0; i < NUM_REQUEST_HANDLER_THREADS; i++) {
-        pthread_create(&threads[i], NULL, handle_requests, NULL);
-    }
+    printf("server listening on port %d\n", ntohs(server_address.sin_port));
 
-    printf("server listening on port %d\n", server_port);
+    while (1) {
+        struct sockaddr_in client_address;
+        socklen_t client_address_len = sizeof(client_address);
 
-    for (int i = 0; i < NUM_REQUEST_HANDLER_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-
-    return 0;
-}
-
-static int read_stream(int fd, void *buf, unsigned int length) {
-    unsigned int total = 0;
-
-    while (total < length) {
-        int n = read(fd, (char *) buf + total, length - total);
-        if (n <= 0) {
-            return -1;
+        int client_socket = accept(server_socket, (struct sockaddr *) &client_address, &client_address_len);
+        if (client_socket < 0) {
+            // TODO: error handling
+            continue;
         }
 
-        total += n;
+        int *client_socket_copy = malloc(sizeof(int));
+        if (client_socket_copy == NULL) {
+            close(client_socket);
+            continue;
+        } 
+        *client_socket_copy = client_socket;
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, connection_handler, client_socket_copy);
     }
 
     return 0;
@@ -129,6 +112,7 @@ static int read_stream(int fd, void *buf, unsigned int length) {
 struct message *receive_message(int conn_socket) {
     struct message *message = malloc(sizeof (struct message));
     if (message == NULL) {
+        errno = EINVAL;
         return NULL;
     }
 
@@ -140,12 +124,14 @@ struct message *receive_message(int conn_socket) {
 
     if (message->header.length >= MAX_PAYLOAD_LENGTH) {
         free(message);
+        errno = EMSGSIZE;
         return NULL;
     }
 
     message->payload = malloc(message->header.length);
     if (message->payload == NULL) {
         free(message);
+        errno = ENOMEM;
         return NULL;
     }
 
