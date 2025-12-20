166fb7dc531dc0df11f22c2587449f8ba448b9c0
michaelhyi <michaelyi2004@gmail.com>
2025-12-20 15:28:48 -0800

    updates to dmqp code according to new design

--- include/dmqp.h
+++ include/dmqp.h
@@ -7,28 +7,16 @@
 #define DEFAULT_SERVER_PORT 8080
 #define MB (1 << 20)
 #define MAX_PAYLOAD_LENGTH 1 * MB
-#define DMQP_HEADER_SIZE 16
+#define DMQP_HEADER_SIZE 12
 
-enum dmqp_method {
-    DMQP_RESPONSE,
-    DMQP_PUSH,
-    DMQP_POP,
-    DMQP_PEEK,
-    DMQP_PEEK_TIMESTAMP
-};
+enum dmqp_method { DMQP_PUSH, DMQP_POP, DMQP_PEEK_SEQUENCE_ID, DMQP_RESPONSE };
 
 // All header fields are expected to be in network byte order (big endian).
 struct dmqp_header {
-    uint64_t timestamp; // unix epoch, 1 second resolution. set by the
-                        // controller server on a DMQP_PUSH request, indiciating
-                        // the timestamp at which data was received for in-order
-                        // delivery. set by the partition on a DMQP_PEEK request
-                        // to handle in-order popping
-    uint32_t length;    // payload length
-    uint8_t method;     // maps to `enum dmqp_method`
-    uint8_t topic_id;   // id of topic to perform operation on. set by
-                        // controller server.
-    int8_t status_code; // errno
+    uint32_t sequence_id; // unique sequence number of queue entry
+    uint32_t length;      // payload length
+    uint16_t method;      // maps to `enum dmqp_method`
+    int16_t status_code;  // unix errno
 };
 
 struct dmqp_message {
@@ -66,10 +54,10 @@ int dmqp_server_init(unsigned short server_port);
  *
  * @param fd file descriptor to read from
  * @param buf DMQP message buffer to write to
- * @returns the number of bytes read if success, -1 if error with global `errno`
+ * @returns 0 on success, -1 if error with global `errno`
  * set
  */
-ssize_t read_dmqp_message(int fd, struct dmqp_message *buf);
+int read_dmqp_message(int fd, struct dmqp_message *buf);
 
 /**
  * Sends a DMQP message to a socket. Converts header fields to network byte
@@ -82,11 +70,10 @@ ssize_t read_dmqp_message(int fd, struct dmqp_message *buf);
  * @param socket socket to write to
  * @param buffer DMQP message buffer to send
  * @param flags same flags param as send syscall
- * @returns the number of bytes sent if success, -1 if error with global `errno`
+ * @returns 0 on success, -1 if error with global `errno`
  * set
  */
-ssize_t send_dmqp_message(int socket, const struct dmqp_message *buffer,
-                          int flags);
+int send_dmqp_message(int socket, const struct dmqp_message *buffer, int flags);
 
 /**
  * Handles a message received at a DMQP server by reading the message
@@ -100,20 +87,21 @@ ssize_t send_dmqp_message(int socket, const struct dmqp_message *buffer,
  */
 int handle_dmqp_message(int socket);
 
-// ----------------------------------------------------------------------------
-// The following functions must be implemented separately by each DMQP server.
-
 /**
- * Handles a DMQP message with method `DMQP_RESPONSE`.
+ * Handles a DMQP message with unknown method.
  *
- * Throws an error if `message` is null, the method is not `DMQP_RESPONSE`, or
+ * Throws an error if `message` is null, the method is not unknown, or
  * `reply_socket` is invalid.
  *
  * @param message message received by server
  * @param reply_socket socket to reply on
  * @returns 0 if success, -1 if error with global `errno` set
  */
-int handle_dmqp_response(const struct dmqp_message *message, int reply_socket);
+int handle_dmqp_unknown_method(const struct dmqp_message *message,
+                               int reply_socket);
+
+// ----------------------------------------------------------------------------
+// The following functions must be implemented separately by each DMQP server.
 
 /**
  * Handles a DMQP message with method `DMQP_PUSH`.
@@ -140,41 +128,28 @@ int handle_dmqp_push(const struct dmqp_message *message, int reply_socket);
 int handle_dmqp_pop(const struct dmqp_message *message, int reply_socket);
 
 /**
- * Handles a DMQP message with method `DMQP_PEEK`.
- *
- * Throws an error if `message` is null, the method is not `DMQP_PEEK`, or
- * `reply_socket` is invalid.
- *
- * @param message message received by server
- * @param reply_socket socket to reply on
- * @returns 0 if success, -1 if error with global `errno` set
- */
-int handle_dmqp_peek(const struct dmqp_message *message, int reply_socket);
-
-/**
- * Handles a DMQP message with method `DMQP_PEEK_TIMESTAMP`.
+ * Handles a DMQP message with method `DMQP_PEEK_SEQUENCE_ID`.
  *
  * Throws an error if `message` is null, the method is not
- * `DMQP_PEEK_TIMESTAMP`, or `reply_socket` is invalid.
+ * `DMQP_PEEK_SEQUENCE_ID`, or `reply_socket` is invalid.
  *
  * @param message message received by server
  * @param reply_socket socket to reply on
  * @returns 0 if success, -1 if error with global `errno` set
  */
-int handle_dmqp_peek_timestamp(const struct dmqp_message *message,
-                               int reply_socket);
+int handle_dmqp_peek_sequence_id(const struct dmqp_message *message,
+                                 int reply_socket);
 
 /**
- * Handles a DMQP message with unknown method.
+ * Handles a DMQP message with method `DMQP_RESPONSE`.
  *
- * Throws an error if `message` is null, the method is not unknown, or
+ * Throws an error if `message` is null, the method is not `DMQP_RESPONSE`, or
  * `reply_socket` is invalid.
  *
  * @param message message received by server
  * @param reply_socket socket to reply on
  * @returns 0 if success, -1 if error with global `errno` set
  */
-int handle_dmqp_unknown_method(const struct dmqp_message *message,
-                               int reply_socket);
+int handle_dmqp_response(const struct dmqp_message *message, int reply_socket);
 
 #endif
--- lib/dmqp.c
+++ lib/dmqp.c
@@ -8,26 +8,6 @@
 
 #include "network.h"
 
-#if defined(__linux__)
-#include <endian.h>
-#define ntohll(x) be64toh(x)
-#define htonll(x) htobe64(x)
-#elif defined(__APPLE__) || defined(__FreeBSD__)
-#include <sys/endian.h>
-#define ntohll(x) be64toh(x)
-#define htonll(x) htobe64(x)
-#else // Fallback implementation
-#if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-#define ntohll(x)                                                              \
-    ((((uint64_t)ntohl((uint32_t)(x))) << 32) | ntohl((uint32_t)((x) >> 32)))
-#define htonll(x)                                                              \
-    ((((uint64_t)htonl((uint32_t)(x))) << 32) | htonl((uint32_t)((x) >> 32)))
-#else
-#define ntohll(x) (x)
-#define htonll(x) (x)
-#endif
-#endif
-
 /**
  * Reads a DMQP header from a file descriptor. Converts header fields to host
  * byte order.
@@ -36,10 +16,9 @@
  *
  * @param fd file descriptor to read from
  * @param buf DMQP header buffer to write to
- * @returns the number of bytes read if success, -1 if error with global `errno`
- * set
+ * @returns 0 on success, -1 if error with global `errno` set
  */
-static ssize_t read_dmqp_header(int fd, struct dmqp_header *buf) {
+static int read_dmqp_header(int fd, struct dmqp_header *buf) {
     if (fd < 0 || buf == NULL) {
         errno = EINVAL;
         return -1;
@@ -51,18 +30,17 @@ static ssize_t read_dmqp_header(int fd, struct dmqp_header *buf) {
         return -1;
     }
 
-    memcpy(&buf->timestamp, header_wire_buf, 8);
-    memcpy(&buf->length, header_wire_buf + 8, 4);
-    buf->method = header_wire_buf[13];
-    buf->topic_id = header_wire_buf[14];
-    buf->status_code = header_wire_buf[15];
+    memcpy(&buf->sequence_id, header_wire_buf, 4);
+    memcpy(&buf->length, header_wire_buf + 4, 4);
+    buf->method = header_wire_buf[8];
+    buf->status_code = header_wire_buf[10];
 
-    // no need to convert method, topic_id, and status_code since they're only a
-    // byte
-    buf->timestamp = ntohll(buf->timestamp);
+    buf->sequence_id = ntohl(buf->sequence_id);
     buf->length = ntohl(buf->length);
+    buf->method = ntohs(buf->method);
+    buf->status_code = ntohs(buf->status_code);
 
-    return n;
+    return 0;
 }
 
 /**
@@ -74,9 +52,8 @@ static ssize_t read_dmqp_header(int fd, struct dmqp_header *buf) {
  *
  * @param socket socket to write to
  * @param buf DMQP header buffer to send
- * @param flags same flags param as send syscall
- * @returns the number of bytes sent if success, -1 if error with global `errno`
- * set
+ * @param flags same flags param as `send` syscall
+ * @returns 0 on success, -1 if error with global `errno` set
  */
 static ssize_t send_dmqp_header(int socket, const struct dmqp_header *buffer,
                                 int flags) {
@@ -90,24 +67,23 @@ static ssize_t send_dmqp_header(int socket, const struct dmqp_header *buffer,
         return -1;
     }
 
-    // no need to convert method, topic_id, and status_code since they're only a
-    // byte
-    uint64_t network_byte_ordered_timestamp = htonll(buffer->timestamp);
+    uint32_t network_byte_ordered_sequence_id = htonl(buffer->sequence_id);
     uint32_t network_byte_ordered_length = htonl(buffer->length);
+    uint16_t network_byte_ordered_method = htons(buffer->method);
+    int16_t network_byte_ordered_status_code = htons(buffer->status_code);
 
     char header_wire_buf[DMQP_HEADER_SIZE];
-    memcpy(header_wire_buf, &network_byte_ordered_timestamp, 8);
-    memcpy(header_wire_buf + 8, &network_byte_ordered_length, 4);
-    header_wire_buf[13] = buffer->method;
-    header_wire_buf[14] = buffer->topic_id;
-    header_wire_buf[15] = buffer->status_code;
+    memcpy(header_wire_buf, &network_byte_ordered_sequence_id, 4);
+    memcpy(header_wire_buf + 4, &network_byte_ordered_length, 4);
+    header_wire_buf[8] = network_byte_ordered_method;
+    header_wire_buf[10] = network_byte_ordered_status_code;
 
     ssize_t n = send_all(socket, header_wire_buf, DMQP_HEADER_SIZE, flags);
-    if (n < DMQP_HEADER_SIZE) {
+    if (n < 0) {
         return -1;
     }
 
-    return n;
+    return 0;
 }
 
 int dmqp_client_init(const char *server_host, unsigned short server_port) {
@@ -133,14 +109,14 @@ int dmqp_server_init(unsigned short server_port) {
     return 0;
 }
 
-ssize_t read_dmqp_message(int fd, struct dmqp_message *buf) {
+int read_dmqp_message(int fd, struct dmqp_message *buf) {
     if (fd < 0 || buf == NULL) {
         errno = EINVAL;
         return -1;
     }
 
-    int res = read_dmqp_header(fd, &buf->header);
-    if (res < 0) {
+    ssize_t n = read_dmqp_header(fd, &buf->header);
+    if (n < DMQP_HEADER_SIZE) {
         return -1;
     }
 
@@ -151,7 +127,7 @@ ssize_t read_dmqp_message(int fd, struct dmqp_message *buf) {
 
     if (buf->header.length == 0) {
         buf->payload = NULL;
-        return DMQP_HEADER_SIZE;
+        return 0;
     }
 
     buf->payload = malloc(buf->header.length);
@@ -160,17 +136,17 @@ ssize_t read_dmqp_message(int fd, struct dmqp_message *buf) {
         return -1;
     }
 
-    ssize_t n = read_all(fd, buf->payload, buf->header.length);
+    n = read_all(fd, buf->payload, buf->header.length);
     if (n < buf->header.length) {
         free(buf->payload);
         return -1;
     }
 
-    return DMQP_HEADER_SIZE + buf->header.length;
+    return 0;
 }
 
-ssize_t send_dmqp_message(int socket, const struct dmqp_message *buffer,
-                          int flags) {
+int send_dmqp_message(int socket, const struct dmqp_message *buffer,
+                      int flags) {
     if (socket < 0 || buffer == NULL ||
         (buffer->header.length > 0 && buffer->payload == NULL)) {
         errno = EINVAL;
@@ -182,21 +158,21 @@ ssize_t send_dmqp_message(int socket, const struct dmqp_message *buffer,
         return -1;
     }
 
-    int res = send_dmqp_header(socket, &buffer->header, flags);
-    if (res < 0) {
+    ssize_t n = send_dmqp_header(socket, &buffer->header, flags);
+    if (n < DMQP_HEADER_SIZE) {
         return -1;
     }
 
     if (buffer->header.length == 0) {
-        return DMQP_HEADER_SIZE;
+        return 0;
     }
 
-    ssize_t n = send_all(socket, buffer->payload, buffer->header.length, flags);
+    n = send_all(socket, buffer->payload, buffer->header.length, flags);
     if (n < buffer->header.length) {
         return -1;
     }
 
-    return DMQP_HEADER_SIZE + buffer->header.length;
+    return 0;
 }
 
 int handle_dmqp_message(int socket) {
@@ -212,20 +188,17 @@ int handle_dmqp_message(int socket) {
     }
 
     switch (buf.header.method) {
-    case DMQP_RESPONSE:
-        res = handle_dmqp_response(&buf, socket);
-        break;
     case DMQP_PUSH:
         res = handle_dmqp_push(&buf, socket);
         break;
     case DMQP_POP:
         res = handle_dmqp_pop(&buf, socket);
         break;
-    case DMQP_PEEK:
-        res = handle_dmqp_peek(&buf, socket);
+    case DMQP_PEEK_SEQUENCE_ID:
+        res = handle_dmqp_peek_sequence_id(&buf, socket);
         break;
-    case DMQP_PEEK_TIMESTAMP:
-        res = handle_dmqp_peek_timestamp(&buf, socket);
+    case DMQP_RESPONSE:
+        res = handle_dmqp_response(&buf, socket);
         break;
     default:
         res = handle_dmqp_unknown_method(&buf, socket);
@@ -237,4 +210,29 @@ int handle_dmqp_message(int socket) {
     }
 
     return res;
+}
+
+int handle_dmqp_unknown_method(const struct dmqp_message *message,
+                               int reply_socket) {
+    if (message == NULL || reply_socket < 0) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    struct dmqp_header res_header = {
+        .sequence_id = 0,
+        .length = 0,
+        .method = DMQP_RESPONSE,
+
+        .status_code = ENOSYS,
+    };
+    struct dmqp_message res_message = {.header = res_header};
+
+    ssize_t n = send_dmqp_message(reply_socket, &res_message, 0);
+    if (n < DMQP_HEADER_SIZE) {
+        return -1;
+    }
+
+    errno = ENOSYS;
+    return 0;
 }
