6fd1fafa336a1cfbe28d5bb1e863d40aa56de5ba
michaelhyi <michaelyi2004@gmail.com>
2025-12-21 17:12:41 -0800

    update include/api.h with error descriptions

--- include/api.h
+++ include/api.h
@@ -21,13 +21,14 @@ struct message {
  * Creates a topic in the distributed message queue, allocating partitions as
  * requested.
  *
- * Throws an error if arguments are null, connections to the distributed
- * system's servers fail, the topic already exists, or partition allocation
- * fails.
- *
  * @param server the distributed system's metadata server
  * @param topic the topic to create
  * @returns 0 if success, -1 if error with global `errno` set
+ * @throws `EINVAL` invalid arguments
+ * @throws `EEXIST` topic already exists
+ * @throws `ENODEV` partition allocation failure
+ * @throws `EIO` internal service error, likely connection to distributed system
+ * servers fail
  */
 int create_topic(const struct server *server, const struct topic *topic);
 
@@ -35,13 +36,14 @@ int create_topic(const struct server *server, const struct topic *topic);
  * Updates a topic in the distributed message queue, updating partition
  * allocation as requested.
  *
- * Throws an error if arguments are null, connections to the distributed
- * system's servers fail, the topic does not exist, or partition allocation
- * fails.
- *
  * @param server the distributed system's metadata server
  * @param topic the topic to update
  * @returns 0 if success, -1 if error with global `errno` set
+ * @throws `EINVAL` invalid arguments
+ * @throws `ENODATA` topic does not exist
+ * @throws `ENODEV` partition allocation failure
+ * @throws `EIO` internal service error, likely connection to distributed system
+ * servers fail
  */
 int update_topic(const struct server *server, const struct topic *topic);
 
@@ -49,25 +51,27 @@ int update_topic(const struct server *server, const struct topic *topic);
  * Deletes a topic in the distributed message queue, freeing all allocated
  * partitions.
  *
- * Throws an error if arguments are null, connections to the distributed
- * system's servers fail, or the topic does not exist.
- *
  * @param server the distributed system's metadata server
  * @param topic_name name of the topic to delete
  * @returns 0 if success, -1 if error with global `errno` set
+ * @throws `EINVAL` invalid arguments
+ * @throws `ENODATA` topic does not exist
+ * @throws `EIO` internal service error, likely connection to distributed system
+ * servers fail
  */
 int delete_topic(const struct server *server, const char *topic_name);
 
 /**
  * Registers the current process as a consumer of the distributed message queue.
  *
- * Throws an error if arguments are null, connections to the distributed
- * system's servers fail, the topic does not exist, or the topic's consumer
- * limit has been reached.
- *
  * @param server the distributed system's metadata server
  * @param topic_name name of the topic to delete
  * @returns 0 if success, -1 if error with global `errno` set
+ * @throws `EINVAL` invalid arguments
+ * @throws `ENODATA` topic does not exist
+ * @throws `EMFILE` topic's consumer limit reached
+ * @throws `EIO` internal service error, likely connection to distributed system
+ * servers fail
  */
 int consumer_init(const struct server *server, const char *topic_name);
 
@@ -75,13 +79,14 @@ int consumer_init(const struct server *server, const char *topic_name);
  * Pushes a message onto the distributed message queue by using Round-Robin to
  * distribute between shards.
  *
- * Throws an error if arguments are null, connections to the distributed
- * system's servers fail, or the topic does not exist.
- *
  * @param server the distributed system's metadata server
  * @param topic_name name of the topic to push to
  * @param buf the message to push
  * @returns 0 if success, -1 if error with global `errno` set
+ * @throws `EINVAL` invalid arguments
+ * @throws `ENODATA` topic does not exist
+ * @throws `EIO` internal service error, likely connection to distributed system
+ * servers fail
  */
 int push(const struct server *server, const char *topic_name,
          const struct message *buf);
@@ -90,14 +95,15 @@ int push(const struct server *server, const char *topic_name,
  * Pops a message from the distributed message queue, only popping from the
  * shard assigned to the consumer.
  *
- * Throws an error if arguments are null, connections to the distributed
- * system's servers fail, the topic does not exist, or the current process is
- * not assigned a shard from this topic.
- *
  * @param server the distributed system's metadata server
  * @param topic_name name of the topic to pop from
  * @param buf output param to return the message
  * @returns 0 if success, -1 if error with global `errno` set
+ * @throws `EINVAL` invalid arguments
+ * @throws `ENODATA` topic does not exist
+ * @throws `EACCES` current process not assigned as a consumer of this topic
+ * @throws `EIO` internal service error, likely connection to distributed system
+ * servers fail
  */
 int pop(const struct server *server, const char *topic_name,
         struct message *buf);
